64118814fd21a487b529c2d903e9ab1702ba0646
MatteRubbbiani <matte.rubbiani@gmail.com>
2021-02-03 21:02:41 +0100

    H

--- src/components/LoginPopup.vue
+++ src/components/LoginPopup.vue
@@ -34,7 +34,7 @@ export default {
       }
     },
     redirectToLogin(){
-      window.location.href = "/auth/login";
+      window.location.href = "http://unseen.papero.tk/auth/google";
     }
   }
 }
