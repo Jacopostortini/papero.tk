f0a67ff92b32e4e085d80427106762813c4da930
matteo <matte.rubbiani@gmail.com>
2021-04-01 09:57:55 +0200

    H

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 const baseUrl = "https://papero.me";
 const urls = {
     getLoginInfoUrl: baseUrl+"/user/get_info",
-    logoutUrl: baseUrl+"auth/logout"
+    logoutUrl: baseUrl+"/auth/logout"
 }
 export {
     urls
