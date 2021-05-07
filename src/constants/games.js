f997ad9912a767de3d37ac0d5da7b9760097081a
matteo <matte.rubbiani@gmail.com>
2021-05-07 11:08:34 +0200

    H

--- src/constants/games.js
+++ src/constants/games.js
@@ -37,12 +37,14 @@ export default [
      {
          name: "DUCKTAN",
          url: "/ducktan",
-         image: require("@/assets/gamesImages/COMINGSOON_background.png"),
+         image: require("@/assets/gamesImages/DUCKTAN_background.png"),
          title: require("@/assets/gamesImages/DUCKTAN_title.png"),
+         preview: require("@/assets/gamesImages/DUCKTAN_preview.png"),
+
          gridColumn: 2,
          gridRow: 4,
          color: "$theme-color",
-         available: false
+         available: true
       },
      {
          name: "COMING SOON",
